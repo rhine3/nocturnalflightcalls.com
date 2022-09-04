7c6283f52045ceea0108da80ce966590ddd9b133
Tessa Rhinehart <tessa.rhinehart@pitt.edu>
2022-09-04 17:07:53 -0400

    Update denoising

--- code/2_nfc_extraction/nfc_extraction.py
+++ code/2_nfc_extraction/nfc_extraction.py
@@ -1,7 +1,12 @@
-"""# NFC extraction testing
+#!/usr/bin/env python
+# coding: utf-8
 
-Short tests of script to extract NFCs.
-"""
+# # NFC extraction testing
+# 
+# Short tests of script to extract NFCs.
+
+# In[1]:
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,16 +17,30 @@ import pandas as pd
 from pathlib import Path
 import random
 
+
 # Load the document which describes the upper and lower frequency limits for bandpassing as well as the approximate duration of the call
+
+# In[2]:
+
+
 freq_limits = pd.read_csv("../freqs_and_durations_manual_edits.csv")
-#freq_limits.head()
+freq_limits.head()
+
 
 # Load the table which describes the pairings between annotation files and audio files
+
+# In[3]:
+
+
 tables_to_filenames = pd.read_csv("../1_nfc_annotation_prep/annotation_audio_pairs.csv", index_col='annotation_file').to_dict()['audio_file']
 
-# -----------------------------------
-# Functions for creating spectrograms
-# -----------------------------------
+
+# ## Functions for creating spectrograms
+# 
+# Formatting function to nicely format ticks for frequency (y axis) and time (x axis)
+
+# In[4]:
+
 
 def freq_reformatter(tick_val_hertz, pos):
     """
@@ -34,6 +53,10 @@ def freq_reformatter(tick_val_hertz, pos):
         str_format = str_format + '  '
     return str_format
 
+
+# In[5]:
+
+
 def sec_reformatter(tick_val_sec, pos):
     """
     Input: float value in seconds, e.g. 0.15
@@ -41,9 +64,17 @@ def sec_reformatter(tick_val_sec, pos):
     """
     val_ms = tick_val_sec*1000
     return "%.0f" % val_ms
+    
+
+
+# Function to save nice-looking spectrograms (using the functions above for formatting)
+
+# In[8]:
+
 
 plt.rcParams['figure.dpi'] = 300
-def save_or_plot_spectrogram(s, filename, duration, save=True, pixels_tall=150):
+
+def save_or_plot_spectrogram(s, filename, duration, save=True, pixels_tall=133, short_spec=True):
     """Create and save an NFC spectrogram
     
     Inputs:
@@ -53,7 +84,11 @@ def save_or_plot_spectrogram(s, filename, duration, save=True, pixels_tall=150):
         save: whether or not to save the file. If not saved, just plots the fig
     """
     px = 1/plt.rcParams['figure.dpi']  # pixel in inches
-    plt.subplots(figsize=(duration*pixels_tall*5*px, pixels_tall*px))
+    if short_spec:
+        plt.subplots(figsize=(duration*pixels_tall*5*px, pixels_tall*px))
+    else:
+        plt.subplots(figsize=(duration*pixels_tall*2*px, pixels_tall*px))
+        
     # plt.subplots(figsize=(duration*30, 5))
     
     plot = s.plot(inline=False)
@@ -67,12 +102,15 @@ def save_or_plot_spectrogram(s, filename, duration, save=True, pixels_tall=150):
     ax.spines['top'].set_visible(False)
     ax.margins(x=0)
     
-    ax.set_xticks(np.arange(0, duration, 0.05)[1:])
+    if short_spec:
+        ax.set_xticks(np.arange(0, duration-0.025, 0.05)[1:])
+    else:
+        ax.set_xticks(np.arange(0, duration-0.25, 0.5)[1:])
+        
     ax.set_yticks([2000, 4000, 6000, 8000, 10000])
     
-    
     for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(0.5)
+        ax.spines[axis].set_linewidth(0.2)
     
     ax.xaxis.set_major_formatter(ticker.FuncFormatter(sec_reformatter))
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(freq_reformatter))
@@ -82,12 +120,18 @@ def save_or_plot_spectrogram(s, filename, duration, save=True, pixels_tall=150):
         plt.show()
     plt.close()
 
-# -----------------------------------------------------------------
-# Create table of all annotations and subsample it
-# This allows us to take annotations from a random set of files.
-# -----------------------------------------------------------------
 
-# Create table of annotations from all annotation files
+# ## Create table of all annotations and subsample it
+# 
+# This allows us to take annotations from a random set of files.
+
+# ### Create table of annotations from all annotation files
+# 
+# This step takes a while...
+
+# In[9]:
+
+
 keys = list(tables_to_filenames.keys()) 
 all_annots_list = []
 
@@ -106,16 +150,33 @@ all_annots_df = pd.concat(all_annots_list)
 all_annots_df.reset_index(drop=True, inplace=True)
 
 
-# Select a subset of the annotations for each species
+# ### Select a subset of the annotations for each species
 
-no_qs = all_annots_df[all_annots_df["Alpha code"] != '?'] # Remove unknowns
-shuffled = no_qs.sample(frac=1, random_state=42) # Shuffle table
+# Remove unknowns
 
-# Recordings we've decided to exclude 
-#(possible TODO: change this so we can exclude individual calls)
-exclusions = pd.read_csv("exclusions.csv").filename_no_extension.tolist() 
+# In[10]:
 
-# Get the indices of needed annotations, a maximum of 30
+
+no_qs = all_annots_df[all_annots_df["Alpha code"] != '?']
+shuffled = no_qs.sample(frac=1, random_state=42)
+
+
+# Calls that we've decided should be excluded due to quality, etc.
+
+# In[11]:
+
+
+exclusions = pd.read_csv("exclusions.csv").filename_no_extension.tolist()
+exclusions
+
+
+# Get the indices of needed annotations
+# 
+# Takes a while...
+
+# In[12]:
+
+
 code_counts = {}
 max_samples = 30
 idxs = []
@@ -133,26 +194,44 @@ for idx, row in shuffled.iterrows():
         idxs.append(idx)
 idxs.sort()
 
-# Subset those indices from the main table
+
+# In[13]:
+
+
+len(idxs)
+
+
+# In[14]:
+
+
+idxs[:5]
+
+
+# Subset those indices from the main table.
+
+# In[15]:
+
+
 sampled_calls = all_annots_df.iloc[idxs]
+sampled_calls.head()
 
 
+# ## Extraction!
 
-# -----------------------------------------------------------------
-# Extract the sounds and recordings
-# This allows us to take annotations from a random set of files.
-# -----------------------------------------------------------------
+# ### Spectrogram creation parameters
+
+# In[16]:
+
 
 # Spectrogram creation parameters
-#sample_rate=22050
+sample_rate = 24000 # What many of the source files are to start with
 window_samples = 256
 overlap_samples = int(window_samples*0.9)
 window_type = 'blackman'
 decibel_limits = (-80, 0)
 
 # Denoising parameters
-#denoise=True
-#quantile=0.8
+denoise_quantile=0.8
 bandpass = True
 bp_order = 4
 
@@ -160,14 +239,17 @@ bp_order = 4
 max_num_examples = 30
 create_audio = True
 save_audio = True
-save_spectrogram = True
+save_long_spectrogram = True
+save_short_spectrogram = True
 
 # Recording length information
 buffer_long_recording_seconds = 1
-buffer_short_recording_multiplier = 0.7
+buffer_short_recording_multiplier = 1
 
 
-# Needed function for figuring out what to extract
+# In[17]:
+
+
 def find_segment_start_and_duration(
     begin, end, 
     final_duration=None,
@@ -221,7 +303,25 @@ def find_segment_start_and_duration(
     duration = species_call_duration + buffer*2
     return start, duration
 
-media_path = Path("../../../assets/media")
+print(
+    find_segment_start_and_duration(2.5, 3.5, final_duration = 6),
+    find_segment_start_and_duration(11, 19, buffer_length=1, species_call_duration=8),
+    find_segment_start_and_duration(3, 5, buffer_percent=0.5, species_call_duration=2)
+)
+
+
+# ### Create and save spectrograms and audio
+# 
+# Output for each of the sampled annotations is named after the original clip name, plus the following extensions:
+# * `.wav` - bandpassed wav for long segment (long enough that listener's ear can adjust to noise before sound)
+# * `.jpg` - bandpassed jpg for long segment
+# * `_display.jpg` - jpg for long segment, bandpassed but not denoised
+# * `_display_denoised.jpg` - jpg for long segment, bandpassed and denoised.
+
+# In[18]:
+
+
+media_path = Path("../../media")
 media_path.mkdir(exist_ok=True)
 metadata = []
 
@@ -241,10 +341,6 @@ for idx, row in sampled_calls.reset_index(drop=True).iterrows():
     dirname.mkdir(exist_ok=True)
     filename = '_'.join([txt_file_path.stem, *file_info])
 
-    if alpha in ["BLGR", "BRCR", "COGO", "EVGR", "LALO", "OROR", "RBGU", "SORA", "STSA"]:
-        print()
-        print(alpha, txt_file_name)
-
     print(filename)
 
     # Create a longer audio segment and spectrogram to save
@@ -256,7 +352,7 @@ for idx, row in sampled_calls.reset_index(drop=True).iterrows():
             begin, end,
             buffer_length=buffer_long_recording_seconds,
             species_call_duration=approx_duration)
-        long_segment = Audio.from_file(audio_file, offset=long_seg_start, duration=long_seg_dur)
+        long_segment = Audio.from_file(audio_file, sample_rate=sample_rate, offset=long_seg_start, duration=long_seg_dur)
         
         # Bandpass samples
         if bandpass:
@@ -270,12 +366,12 @@ for idx, row in sampled_calls.reset_index(drop=True).iterrows():
         if save_audio:
             audio_filename = str(audio_dirname.joinpath(filename)) + '.wav'
             long_segment.save(audio_filename)
-            """s = Spectrogram.from_audio(
+            s = Spectrogram.from_audio(
                 long_segment, window_samples=window_samples, overlap_samples=overlap_samples, decibel_limits=decibel_limits, window_type=window_type)
             spectrogram_dirname = dirname.joinpath('spectrograms')
             spectrogram_dirname.mkdir(exist_ok=True)
             spectrogram_filename = str(spectrogram_dirname.joinpath(filename)) + '.jpg'
-            save_or_plot_spectrogram(s, filename=str(spectrogram_filename), duration=long_seg_dur, save=save_spectrogram)"""
+            save_or_plot_spectrogram(s, filename=str(spectrogram_filename), duration=long_seg_dur, save=save_long_spectrogram, short_spec=False)
 
 
     # Save some info about the file
@@ -286,21 +382,45 @@ for idx, row in sampled_calls.reset_index(drop=True).iterrows():
         begin, end,
         buffer_percent = buffer_short_recording_multiplier,
         species_call_duration=approx_duration)
-
-    short_segment = Audio.from_file(audio_file, offset=short_seg_start, duration=short_seg_dur)
+    
+    short_segment = Audio.from_file(audio_file, sample_rate=sample_rate, offset=short_seg_start, duration=short_seg_dur)
+    
+    # Normalize samples
+    neg_3_db = 0.7080078
+    multiplier = neg_3_db / max(abs(short_segment.samples))
+    short_segment.samples = short_segment.samples * multiplier
+    
     if bandpass:
         short_segment = short_segment.bandpass(bp_low, bp_high, order=bp_order)
 
     s = Spectrogram.from_audio(
         short_segment, window_samples=window_samples, overlap_samples=overlap_samples, decibel_limits=decibel_limits, window_type=window_type)
+    
     spectrogram_dirname = dirname.joinpath('spectrograms')
     spectrogram_dirname.mkdir(exist_ok=True)
-    spectrogram_filename = str(spectrogram_dirname.joinpath(filename)) + '_display.jpg'
-    save_or_plot_spectrogram(s, filename=str(spectrogram_filename), duration=short_seg_dur, save=save_spectrogram)
-
+    spectrogram_filename_regular = str(spectrogram_dirname.joinpath(filename)) + '_display.jpg'
+    spectrogram_filename_denoise = str(spectrogram_dirname.joinpath(filename)) + '_display_denoise.jpg'
     
-# Save records of clips
+    save_or_plot_spectrogram(s, filename=str(spectrogram_filename_regular), duration=short_seg_dur, save=save_short_spectrogram)
+    
+    quantile = np.quantile(s.spectrogram, q=denoise_quantile)
+    denoised_spec = s.limit_db_range(quantile, 0).min_max_scale((-80, -3))
+    save_or_plot_spectrogram(
+        denoised_spec,
+        filename=str(spectrogram_filename_denoise), duration=short_seg_dur, save=save_short_spectrogram
+    )
+
+# ## Save record of clips
+
+# In[20]:
+
+
 metadata_df = pd.DataFrame(metadata, columns=[
     'project', 'source_clip', 'order', 'family', 'genus', 'species', 'alpha_code',
     'annotation_start_time', 'annotation_end_time', 'audio_clip_start_time'])
 metadata_df.to_csv("clip_metadata.csv")
+
+
+
+
+
